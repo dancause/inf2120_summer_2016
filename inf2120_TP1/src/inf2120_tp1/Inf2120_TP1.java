ed46939cca560759c0159e8d01f944fc3502a4cc
dancause <janindancause@192.168.0.110>
2016-05-18 18:16:22 -0400

    premier commit

--- inf2120_TP1/src/inf2120_tp1/Inf2120_TP1.java
+++ inf2120_TP1/src/inf2120_tp1/Inf2120_TP1.java
@@ -15,6 +15,8 @@ public class Inf2120_TP1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        /*test*/
         // TODO code application logic here
     }
     
